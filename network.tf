6be79937faa268395142ca3527e49f54894f0ee9
stas.kloy <stas.kloy93@gmail.com>
2022-06-14 16:19:56 +0300

    set vnet name and address space to  variables

--- network.tf
+++ network.tf
@@ -1,8 +1,8 @@
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "w5-Vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "eastus"
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_space]
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
