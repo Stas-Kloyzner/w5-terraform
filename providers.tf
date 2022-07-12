38d87bedd73dc6fa0aa5ffdc1d21bcb127c83f26
Stas-Kloyzner <92732559+Stas-Kloyzner@users.noreply.github.com>
2022-07-12 13:36:56 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,21 +1,4 @@
 #root module providers
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-
-  backend "azurerm" {
-    resource_group_name = "storage"
-    storage_account_name = "w5tfstate"
-    container_name = "tfstate"
-  }
-}
-
 provider "azurerm" {
   features {}
 }
