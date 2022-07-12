6c32b91d76fe801b83975675d5c934d7b5209d70
Stas-Kloyzner <92732559+Stas-Kloyzner@users.noreply.github.com>
2022-07-12 13:36:39 +0300

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+
+  backend "azurerm" {
+    resource_group_name = "storage"
+    storage_account_name = "w5tfstate"
+    container_name = "tfstate"
+  }
+}
